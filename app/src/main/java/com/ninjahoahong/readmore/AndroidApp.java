b99556169a98f42e7859aca2c9a0eff7d230c375
Anh Duong <ninjahoahong@users.noreply.github.com>
2017-09-07 05:26:17 +0300

    Add force update

--- app/src/main/java/com/ninjahoahong/readmore/AndroidApp.java
+++ app/src/main/java/com/ninjahoahong/readmore/AndroidApp.java
@@ -2,12 +2,22 @@ package com.ninjahoahong.readmore;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.ninjahoahong.readmore.utils.ForceUpdateChecker;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AndroidApp extends Application {
 
     private static AppComponent appComponent;
     private static AndroidApp INSTANCE;
+
+    private static final String TAG = AndroidApp.class.getSimpleName();
+    private static final long FETCHING_DATA_PERIODICITY_IN_SECOND = 30;
 
     @Override
     public void onCreate() {
@@ -19,6 +29,24 @@ public class AndroidApp extends Application {
                     .netModule(new NetModule(BuildConfig.API_BASE_URL))
                     .build();
         }
+
+        final FirebaseRemoteConfig firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+        // set in-app defaults
+        Map<String, Object> remoteConfigDefaults = new HashMap<>();
+        remoteConfigDefaults.put(ForceUpdateChecker.KEY_UPDATE_REQUIRED, false);
+        remoteConfigDefaults.put(ForceUpdateChecker.KEY_CURRENT_VERSION, "1.0.0");
+        remoteConfigDefaults.put(ForceUpdateChecker.KEY_UPDATE_URL,
+                "https://play.google.com/store/apps/details?id=com.ninjahoahong.readmore");
+
+        firebaseRemoteConfig.setDefaults(remoteConfigDefaults);
+        firebaseRemoteConfig.fetch(FETCHING_DATA_PERIODICITY_IN_SECOND)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "remote config is fetched.");
+                        firebaseRemoteConfig.activateFetched();
+                    }
+                });
     }
 
     public static AppComponent getAppComponent() {
--- app/src/main/java/com/ninjahoahong/readmore/MainActivity.java
+++ app/src/main/java/com/ninjahoahong/readmore/MainActivity.java
@@ -1,25 +1,28 @@
 package com.ninjahoahong.readmore;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.ninjahoahong.readmore.bookslist.views.BooksListViewKey;
+import com.ninjahoahong.readmore.utils.ForceUpdateChecker;
 import com.zhuinden.simplestack.Backstack;
 import com.zhuinden.simplestack.HistoryBuilder;
 import com.zhuinden.simplestack.StateChange;
 import com.zhuinden.simplestack.navigator.DefaultStateChanger;
 import com.zhuinden.simplestack.navigator.Navigator;
 
-import javax.inject.Inject;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ForceUpdateChecker.OnUpdateNeededListener {
 
     private static final String TAG = "MainActivity";
 
@@ -36,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
                 .setStateChanger(DefaultStateChanger.create(this, appContainer))
                 .install(this, appContainer, HistoryBuilder.single(
                         BooksListViewKey.create()));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ForceUpdateChecker.with(this).onUpdateNeeded(this).check();
     }
 
     @Override
@@ -72,5 +81,23 @@ public class MainActivity extends AppCompatActivity {
             return this;
         }
         return super.getSystemService(name);
+    }
+
+    @Override
+    public void onUpdateNeeded(String updateUrl) {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("New version available")
+                .setMessage("Please, update app to new version to continue reposting.")
+                .setCancelable(false)
+                .setPositiveButton("Update",
+                        (dialog1, which) -> redirectStore(updateUrl)).setNegativeButton("No, thanks",
+                        (dialog12, which) -> finish()).create();
+        dialog.show();
+    }
+
+    private void redirectStore(String updateUrl) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(updateUrl));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
